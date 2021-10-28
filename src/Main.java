4ed45debb7a846b61540ba671245c4128e1efca8
Sveta Kulik <ya.sveta@bk.ru>
2021-10-28 11:09:54 +0300

    Add src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        int bonusAccount = 20;
+        int miles = ticketPrice / bonusAccount;
+        System.out.println(miles);
+    }
+}
